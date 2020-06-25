9d30f766cf1e3b224b2783f1ec56e015fef6aa1f
olgaaktas <binpolyglotte@gmail.com>
2020-06-25 11:11:13 +0300

    added description formatted (username in bold and max characters limit)

--- app/src/main/java/com/example/android/instaclone/network/Post.kt
+++ app/src/main/java/com/example/android/instaclone/network/Post.kt
@@ -1,15 +1,24 @@
 package com.example.android.instaclone.network
 
 import com.squareup.moshi.Json
+import descriptionFormatted
 
 data class Post(
         val id: String,
-        val alt_description: String?,
+        val alt_description: String? = "нет описания",
         val description: String?,
         val likes: Long,
-        @Json(name = "urls") val url: Url
-)
+        @Json(name = "urls") val url: Url,
+        val user: User
+){
+    var likesString = "Нравится: ${likes.toString()}"
+    val descriptionFormatedString = descriptionFormatted(user.username, description, alt_description)
+}
 
 data class Url(
         @Json(name = "small")  val imgScrUrl: String
+)
+
+data class User (
+        val username: String
 )
